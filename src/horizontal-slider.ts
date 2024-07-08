b1cdd846b1bbf4075f2a90c82b27d93ce258eadc
Ashish Kulkarni <ashishkulkarni19999@gmail.com>
2024-07-08 20:11:22 +0530

    plugin done

--- src/horizontal-slider.ts
+++ src/horizontal-slider.ts
@@ -1,4 +1,5 @@
 import { KulpElement, html, css, register, prop } from 'kulp-kit';
+import { state } from 'lit/decorators.js';
 
 @register("horizontal-slider-cards.horizontal-slider")
 export class HorizontalCardsSlider extends KulpElement {
@@ -8,10 +9,14 @@ export class HorizontalCardsSlider extends KulpElement {
     @prop({ type: Number }) autoScrollSeconds = 2;
     @prop({ type: Number }) itemsToScroll = 1;
     @prop({ type: Boolean }) pauseOnHover = false;
+    @prop({ type: Boolean }) showDots = false;
+
+    @state() currentIndex = 0;
+
+    @state() totalItems = 0;
 
     autoScrollingStarted = false;
     hovering = false;
-
     static styles = css`
         :root { 
             --_icon-size: 15px;
@@ -62,7 +67,6 @@ export class HorizontalCardsSlider extends KulpElement {
             display: flex;
             justify-content: end;
         }
-        /* need a flat rounded button with hover effect */
         .icon-button {
             background-color: transparent;
             border: none;
@@ -73,18 +77,47 @@ export class HorizontalCardsSlider extends KulpElement {
         .icon-button:hover {
             background-color: rgba(0, 0, 0, 0.1);
         }
+        .dots {
+            display: flex;
+            justify-content: center;
+            padding: 10px 0;
+        }
+        .dot {
+            cursor: pointer;
+            height: 10px;
+            width: 10px;
+            margin: 0 5px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.3s;
+        }
+        .dot.active {
+            background-color: #717171;
+        }
     `;
 
     connectedCallback(): void {
         super.connectedCallback();
+        this.getTotalItems();
+        this.updateDots();
         if (this.autoScroll) {
             this.startAutoScrolling(this.autoScrollSeconds);
         }
+        // also add an event listener which says that after each slot change, update the dots
+        const slot = this.shadowRoot?.querySelector("slot");
+        slot?.addEventListener("slotchange", () => {
+            this.getTotalItems();
+            this.updateDots();
+        });
+        setTimeout(() => {this.getTotalItems(); this.updateDots()}, 1000)
     }
 
-    // execute startAutoScrolling after the properties are updated 
+
     updated(changedProperties: Map<string | number | symbol, unknown>): void {
         super.updated(changedProperties);
+        this.getTotalItems();
+        this.updateDots();
         if (changedProperties.has("autoScroll") && this.autoScroll && !this.autoScrollingStarted) {
             this.startAutoScrolling(this.autoScrollSeconds);
         }
@@ -96,7 +129,6 @@ export class HorizontalCardsSlider extends KulpElement {
     }
 
     scrollToLeft() {
-        // scroll the slider container smoothly towards the left only if possible
         const sliderContainer = this.shadowRoot?.querySelector(".slider-container") as HTMLElement;
         if (!sliderContainer) return;
         if (sliderContainer.scrollLeft > 0) {
@@ -105,33 +137,39 @@ export class HorizontalCardsSlider extends KulpElement {
                 behavior: "smooth"
             });
         }
+
+        this.currentIndex = this.currentIndex - this.itemsToScroll;
+        this.updateDots();
+
     }
 
     scrollToRight() {
-        // scroll the slider container smoothly towards the right only if possible
         const sliderContainer = this.shadowRoot?.querySelector(".slider-container") as HTMLElement;
         if (!sliderContainer) return;
+        console.log("Scroll left: ", sliderContainer.scrollLeft)
         if (sliderContainer.scrollLeft < sliderContainer.scrollWidth - sliderContainer.clientWidth) {
             sliderContainer.scrollTo({
                 left: sliderContainer.scrollLeft + (this.getSliderChildWidth() * this.itemsToScroll),
                 behavior: "smooth"
             });
+        } else if (sliderContainer.scrollLeft === sliderContainer.scrollWidth - sliderContainer.clientWidth && this.startAgainAfterEnd) {
+            sliderContainer.scrollTo({
+                left: 0,
+                behavior: "smooth"
+            });
+            this.currentIndex = 0;
+            this.updateDots();
+            return;
         }
-        else {
-            if (sliderContainer.scrollLeft === sliderContainer.scrollWidth - sliderContainer.clientWidth && this.startAgainAfterEnd) {
-                sliderContainer.scrollTo({
-                    left: 0,
-                    behavior: "smooth"
-                });
-            }
-        }
+        this.currentIndex = this.currentIndex + this.itemsToScroll;
+        this.updateDots();
     }
 
     startAutoScrolling(seconds: number) {
         if (this.autoScrollingStarted) return;
         this.scrollToRight();
         setInterval(() => {
-            if (this.pauseOnHover && this.hovering) return
+            if (this.pauseOnHover && this.hovering) return;
             this.scrollToRight();
         }, seconds * 1000);
         this.autoScrollingStarted = true;
@@ -141,21 +179,62 @@ export class HorizontalCardsSlider extends KulpElement {
         this.hovering = value;
     }
 
-    elements = {
-        arrows: html`
-            <div class="arrows">
-                <button class="icon-button" @click=${this.scrollToLeft}><i class="fas fa-chevron-left fa-sm"></i></button>
-                <button class="icon-button" @click=${this.scrollToRight}><i class="fas fa-chevron-right fa-sm"></i></button>            
-            </div>`
+    getTotalItems() {
+        console.log("Getting total items")
+        const slot = this.shadowRoot?.querySelector("slot");
+        const totalItems = slot ? slot.assignedElements().length : 0;
+        console.log(totalItems);
+        this.totalItems = totalItems;
+    }
+
+    updateDots() {
+        const dots = this.shadowRoot?.querySelectorAll('.dot');
+        dots?.forEach((dot, index) => {
+            dot.classList.toggle('active', index === this.currentIndex);
+        });
+    }
+
+    // updateDots() {
+    //     const sliderContainer = this.shadowRoot?.querySelector(".slider-container") as HTMLElement;
+    //     if (!sliderContainer) return;
+    //     this.totalItems = this.getTotalItems();
+
+    //     this.requestUpdate()
+    //     const dots = this.shadowRoot?.querySelectorAll('.dot');
+    //     dots?.forEach((dot, index) => {
+    //         dot.classList.toggle('active', index === this.getCurrentIndex());
+    //     });
+    // }
+
+    handleDotClick(index: number) {
+        const sliderContainer = this.shadowRoot?.querySelector(".slider-container") as HTMLElement;
+        if (!sliderContainer) return;
+        const itemWidth = this.getSliderChildWidth();
+        sliderContainer.scrollTo({
+            left: itemWidth * index,
+            behavior: "smooth"
+        });
+        this.currentIndex = index;
+        this.updateDots();
     }
 
     render() {
         return html`
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-            ${this.showArrows ? this.elements.arrows : null}
+            ${this.showArrows ? html`
+            <div class="arrows">
+                <button class="icon-button" @click=${this.scrollToLeft}><i class="fas fa-chevron-left fa-sm"></i></button>
+                <button class="icon-button" @click=${this.scrollToRight}><i class="fas fa-chevron-right fa-sm"></i></button>            
+            </div>` : null}
             <div class="slider-container" @mouseover=${() => this.setHovering(true)} @mouseout=${() => this.setHovering(false)}>
                 <slot></slot>
             </div>
+            ${this.showDots ? html`
+            <div class="dots">
+                ${Array.from({ length: this.totalItems }).map((_, index) => html`
+                    <span class="dot" @click=${() => this.handleDotClick(index)}></span>
+                `)}
+            </div>` : null}
         `;
     }
 }
